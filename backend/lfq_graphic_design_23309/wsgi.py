0bf8b033e2ee7e74f2329a0cc85d01b0a8268891
Crowdbotics <team@crowdbotics.com>
2020-12-10 21:19:41 +0000

    Update models

--- backend/lfq_graphic_design_23309/wsgi.py
+++ backend/lfq_graphic_design_23309/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lfq_graphic_design_23309.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lfq_graphic_design_23309.settings")
 
 application = get_wsgi_application()
